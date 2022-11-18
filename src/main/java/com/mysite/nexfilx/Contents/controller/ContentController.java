5468aeb356bf113e3328b922ee656252bc7dbe89
nimi2838 <bbongsky11@naver.com>
2022-11-18 18:40:10 +0900

    모달 완성

---
+++ src/main/java/com/mysite/nexfilx/Contents/controller/ContentController.java
@@ -0,0 +1,43 @@
+package com.mysite.nexfilx.Contents.controller;
+
+import com.mysite.nexfilx.Contents.dao.ContentRepository;
+import com.mysite.nexfilx.Contents.domain.NetflixContents;
+import com.mysite.nexfilx.Contents.dto.NetflixDto;
+import com.mysite.nexfilx.Contents.service.ContentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@CrossOrigin("*")
+public class ContentController {
+    @Autowired
+    private final ContentService contentService;
+
+    private final ContentRepository contentRepository;
+
+    @GetMapping("/contents")
+    public List<NetflixDto> getAllContents(@RequestParam("kw") String keyword) {
+        return contentService.getNetflixContentsByContentNum(keyword);
+    }
+
+    @GetMapping("/search/{sk}")
+    public List<NetflixDto> getKeywords(@PathVariable() String sk) {
+
+        return contentService.getNetflixContentsBySearch(sk);
+
+    }
+
+    @GetMapping("/rank")
+    public List<NetflixDto> getRankItems() {
+        return  contentService.getNetflixContentsByContentNum("KR-RANK");
+    }
+
+    @GetMapping("/allcontent")
+    public List<NetflixContents> getContentAll() {
+        return  contentService.getNetflixContents();
+    }
+}
---
+++ src/main/java/com/mysite/nexfilx/Contents/dto/NetflixDto.java
@@ -0,0 +1,54 @@
+package com.mysite.nexfilx.Contents.dto;
+
+import com.mysite.nexfilx.Contents.domain.NetflixContents;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NetflixDto {
+
+    private Long id;
+
+
+    private String contentNum;
+    private String contentName;
+    private int episodes;
+    private String date;
+    private String actor;
+    private int age;
+    private String director;
+
+
+    private String contentImg;
+
+    private String rankingImg;
+    private String contentVideo;
+
+    private String detailImg;
+
+    private String detailTextImg;
+
+    private String mainStory;
+
+    public NetflixDto(NetflixContents netflixContents) {
+        id = netflixContents.getId();
+        contentNum = netflixContents.getContentNum();
+        contentName = netflixContents.getContentName();
+        episodes = netflixContents.getEpisodes();
+        date = netflixContents.getDate();
+        actor = netflixContents.getActor();
+        age = netflixContents.getAge();
+        director = netflixContents.getDirector();
+        contentImg = netflixContents.getContentImg();
+        rankingImg = netflixContents.getRankingImg();
+        contentVideo = netflixContents.getContentVideo();
+        detailImg = netflixContents.getDetailImg();
+        detailTextImg = netflixContents.getDetailTextImg();
+        mainStory = netflixContents.getMainStory();
+    }
+}
