1bb2977fe47f6ba5f331b9f20cc30fcf3a9ea465
nimi2838 <bbongsky11@naver.com>
2022-12-13 18:48:30 +0900

    ãprofile img update

--- src/main/java/com/mysite/nexfilx/User/service/ProfileNameService.java
+++ src/main/java/com/mysite/nexfilx/User/service/ProfileNameService.java
@@ -1,26 +1,32 @@
 package com.mysite.nexfilx.User.service;
 
-import com.mysite.nexfilx.Contents.dto.NetflixDto;
+import com.mysite.nexfilx.User.dao.ImgRepository;
 import com.mysite.nexfilx.User.dao.ProfileRepository;
+import com.mysite.nexfilx.User.domain.ProfileImg;
 import com.mysite.nexfilx.User.domain.ProfileName;
 import com.mysite.nexfilx.User.domain.User;
-import com.mysite.nexfilx.User.dto.UserDto;
+import com.mysite.nexfilx.User.dto.ProfileImgDto;
+import com.mysite.nexfilx.User.dto.ProfileNameDto;
 import com.mysite.nexfilx.User.dto.UserProfileDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ProfileNameService {
 
     private final ProfileRepository profileRepository;
+    private final ImgRepository imgRepository;
 
-    public void setProfileName(User user , ProfileName profileName) {
+    public void setProfileName(User user , ProfileName profileName, ProfileImg profileImg) {
+
         ProfileName profileName2 = ProfileName.builder()
                 .user(user)
                 .nickname(profileName.getNickname())
+                .img(profileImg.getImageUrl())
                 .build();
 
         profileRepository.save(profileName2);
@@ -35,15 +41,22 @@ public class ProfileNameService {
         return userProfileDto;
     }
 
-    public void updateProfile(User user, ProfileName profileName) {
+    public void updateProfile(User user, ProfileNameDto profileNameDto) {
 //        profileRepository.delete(ProfileName.builder()
 //                        .id(profileName.getId())
 //                .build());
-         profileRepository.save(ProfileName.builder()
-                 .id(profileName.getId())
-                 .user(user)
-                 .nickname(profileName.getNickname())
-                 .build());
+        Optional<ProfileName> optionalProfileName = profileRepository.findById(profileNameDto.getId());
+
+        if(optionalProfileName.isPresent()) {
+            ProfileName profileName = optionalProfileName.get();
+            profileRepository.save(ProfileName.builder()
+                    .id(profileName.getId())
+                    .user(user)
+                    .img(profileName.getImg())
+                    .nickname(profileNameDto.getNickname())
+                    .build());
+        }
+
     }
 
     public void deleteProfile(ProfileName profileName) {
@@ -51,4 +64,37 @@ public class ProfileNameService {
                 .id(profileName.getId())
                 .build());
     }
+
+
+    public List<ProfileImg> getImgAll() {
+        return imgRepository.findAll();
+    }
+
+    public ProfileImg findById(String imgId) {
+        return imgRepository.findById(Long.valueOf(imgId)).orElseThrow();
+
+    }
+
+    public void setProfileImg(User user, ProfileImg profileImg, ProfileNameDto profileNameDto) {
+
+        Optional<ProfileName> profileNameId = profileRepository.findById(profileNameDto.getId());
+        if (profileNameId.isPresent()) {
+            ProfileName getprofileName = profileNameId.get();
+
+            ProfileName profileName = ProfileName.builder()
+                    .id(getprofileName.getId())
+                    .user(user)
+                    .nickname(getprofileName.getNickname())
+                    .img(profileImg.getImageUrl())
+                    .build();
+
+            profileRepository.save(profileName);
+        }
+    }
+
+//    public void findByImg(Long id) {
+//        ProfileImg profileImg = imgRepository.findById(id).orElseThrow();
+//        ProfileImg profileImg1 = new ProfileImg(profileImg);
+//        System.out.println("profileImg : "+ profileImg1.getImageUrl());
+//    }
 }
